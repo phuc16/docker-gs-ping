6c456453d224c22c89da0f1ba98b0064250c8a95
phucth <phucth@vincss.local>
2024-02-25 15:36:32 +0700

    update dev

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3 main")
+		return c.HTML(http.StatusOK, "Hello, Docker! <3 dev")
 	})
 
 	e.GET("/health", func(c echo.Context) error {
